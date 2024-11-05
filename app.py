442e7643d810b8b66dd10111c35aec739ceaae6b
Rober G <rgarcia@syntonize.com>
2024-11-05 15:51:13 +0100

    gráfico de líneas

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
-st.title("Hola, Streamlit!")
-st.write("¡Bienvenido a tu primera aplicación de Streamlit!")
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
