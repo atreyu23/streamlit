3f9658ef8a314e8db2b75dfc60f5c5e013f22659
Rober G <rgarcia@syntonize.com>
2024-11-06 13:47:21 +0100

    Añadir título al gráfico

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+st.title("Grafiquito chorra")
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
